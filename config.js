60648bed0c8099feb1985f0887b5bfa0659a6bfa
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:16:46 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THE BLACK KEYS';
+export const nameSongs = [
+	"LITTLE BLACK SUBMARINES", /*1*/
+	"LONELY BOY", /*2*/
+	"TIGHTEN UP", /*3*/
+	"WEIGHT OF LOVE", /*4*/
+	"FEVER", /*5*/
+	"GOLD ON THE CEILING", /*6*/
+	"NEXT GIRL", /*7*/
+	"HOWLIN' FOR YOU", /*8*/
+	"TOO AFRAID TO LOVE YOU", /*9*/
+	"LO/HI", /*10*/
+	"DEAD AND GONE", /*11*/
+	"GO", /*12*/
+	"RUN RIGHT BACK", /*13*/
+	"TURN BLUE", /*14*/
+	"WILD CHILD", /*15*/
+	"YOUR TOUCH", /*16*/
+	"SISTER", /*17*/
+	"GOTTA GET AWAY", /*18*/
+	"CRAWLING KINGSNAKE", /*19*/
+	"SHINE A LITTLE LIGHT", /*20*/
+]
